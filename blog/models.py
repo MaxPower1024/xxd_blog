1467a90f9155c62b1dadb5e77d10f1db907cf409
MaxPower1024 <xd102401@163.com>
2018-04-08 23:57:21 +0800

    修改html模版
    修改html模版

---
+++ blog/models.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.six import python_2_unicode_compatible
+
+# 分类
+@python_2_unicode_compatible
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+# 标签
+@python_2_unicode_compatible
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+
+# 文章
+@python_2_unicode_compatible
+class Post(models.Model):
+    # 标题
+    title = models.CharField(max_length=100)
+
+    # 正文
+    body = models.TextField()
+    # 创建时间
+    created_time = models.DateField()
+    
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    # 自己的blog，不用创建作者模型
+    def __str__(self):
+        return self.title
+    
+    
