dc815aa347c41b070279875dca967aceeb6b2722
MaxPower1024 <xd102401@163.com>
2018-04-09 21:06:16 +0800

    完善评论

--- comments/urls.py
+++ comments/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'comments'
 urlpattern=[
-    url(r'^comment/post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment')
+    url(r'^comment/post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment'),
 ]
