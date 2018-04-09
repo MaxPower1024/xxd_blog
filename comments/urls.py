4c177ba4a6ac0a3cba8b398d9e4d9c796e16f50c
MaxPower1024 <xd102401@163.com>
2018-04-09 23:06:36 +0800

    评论区域有问题

--- comments/urls.py
+++ comments/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'comments'
-urlpattern=[
+urlpatterns=[
     url(r'^comment/post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment'),
 ]
