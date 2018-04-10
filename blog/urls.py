30f6aef3a5d5b18670455d83f0e613c661265b7f
Dapower <xd102401@163.com>
2018-04-10 17:25:26 +0800

    搜索功能有待完善

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^post/(?P<pk>[0-9]+)$', views.PostDetailView.as_view(), name='detail'),
     url(r'^category/(?P<pk>[0-9])/$', views.CategoryView.as_view(), name='category'),
-
+    url(r'^tag/(?P<pk>[0-9])/$', views.TagView.as_view(), name='tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Category
+from .models import Post, Category,Tag
 import markdown
 from comments.forms import CommentForm
 from django.views.generic import ListView, DeleteView
-
+from markdown.extensions.toc import TocExtension
+from django.utils.text import slugify
 
 class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
-
-
-# def detail(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     post.increase_views()
-#     post.body = markdown.markdown(
-#         post.body,
-#         extensions=[
-#             'markdown.extensions.extra',
-#             'markdown.extensions.codehilite',
-#             'markdown.extensions.toc',
-#         ]
-#     )
-#     form = CommentForm()
-#     comment_list = post.comment_set.all()
-#
-#     context = {
-#         'post': post,
-#         'form': form,
-#         'comment_list': comment_list
-#     }
-#     return render(request, 'blog/detail.html', context=context)
 
 
 class PostDetailView(DeleteView):
@@ -42,18 +21,19 @@ class PostDetailView(DeleteView):
         response = super(PostDetailView, self).get(request, *args, **kwargs)
         self.object.increase_views()
         return response
-
+    
     def get_object(self, queryset=None):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
         post = super(PostDetailView, self).get_object(queryset=None)
-        post.body = markdown.markdown(post.body,
-                                      extensions=[
-                                          'markdown.extensions.extra',
-                                          'markdown.extensions.codehilite',
-                                          'markdown.extensions.toc',
-                                      ])
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify)
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
-
+    
     def get_context_data(self, **kwargs):
         # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
         # 还要把评论表单、post 下的评论列表传递给模板。
@@ -65,12 +45,24 @@ class PostDetailView(DeleteView):
             'comment_list': comment_list
         })
         return context
-        
+
+
 class CategoryView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
     
     def get_queryset(self):
-        cate = get_object_or_404(Category, pk=self.kwargs.gets('pk'))
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    
+    def get_queryset(self):
+        tag = get_object_or_404(Tag,pk=self.kwargs.get('pk'))
+        return super(TagView,self).get_queryset().filter(tags=tag)
+    
