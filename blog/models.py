d17d45a10d7c94fe7efb9d2d6f1d9a2dc71b1edd
Dapower <xd102401@163.com>
2018-04-10 12:54:06 +0800

    评论区
    评论区展示无问题，提交评论表格有视觉问题。需要完善，修改完成了类视图

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
 from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
+
+
 # 分类
 @python_2_unicode_compatible
 class Category(models.Model):
@@ -11,6 +15,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 # 标签
 @python_2_unicode_compatible
 class Tag(models.Model):
@@ -18,14 +23,14 @@ class Tag(models.Model):
     
     def __str__(self):
         return self.name
-    
+
 
 # 文章
 @python_2_unicode_compatible
 class Post(models.Model):
     # 标题
     title = models.CharField(max_length=100)
-
+    
     # 正文
     body = models.TextField()
     # 创建时间
@@ -33,11 +38,26 @@ class Post(models.Model):
     
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
+    
+    views = models.PositiveIntegerField(default=0)
+    
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+    
     # 自己的blog，不用创建作者模型
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('blog:detail',kwargs={'pk':self.pk})
+        return reverse('blog:detail', kwargs={'pk': self.pk})
     
+    class Meta:
+        ordering = ['-created_time']
     
+    # def save(self, *args, **kwargs):
+    #     md = markdown.Markdown(extensions=[
+    #         'markdown.extensions.extra',
+    #         'markdown.extensions.codehilite',
+    #     ])
+        
--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9]+)$', views.detail, name='detail'),
-    url(r'^category/(?P<pk>[0-9])/$', views.category, name='category'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^post/(?P<pk>[0-9]+)$', views.PostDetailView.as_view(), name='detail'),
+    url(r'^category/(?P<pk>[0-9])/$', views.CategoryView.as_view(), name='category'),
 
 ]
